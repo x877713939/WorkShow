45eb9bff717efcb9ee60e132c3e3ccbb11eba87a
noon-zl <18228760984@163.com>
2020-11-25 19:31:30 +0800

    banner

---
+++ plugins/WorkshowPlugin/Controller/BannerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WorkshowPlugin\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Controller\AdminV2\BaseController;
+class BannerController extends BaseController
+{
+    public function indexAction()
+    {
+        //拿轮播图数据开始
+        $data = $this->getBlockService()->getBlockByCode('jianmo:home_top_banner');
+        $data = $data['data'];
+        $posters = $data['posters'];//拿首页轮播图数据结束
+        return $this->render('WorkshowPlugin:Banner:banner.html.twig', ['posters' => $posters]);
+    }
+    protected function getBlockService()
+    {
+        return $this->createService('Content:BlockService');
+    }
+
+}
